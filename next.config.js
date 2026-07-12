f11bf155e8f91cdd66be8c16882d03b38f59bc1b
Cizz-29 <diblasifra800@gmail.com>
2026-07-12 15:58:00 +0200

    next.config.js: autorizza flagcdn.com per le immagini bandiera

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ const nextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: 'cdn.sanity.io' },
+      { protocol: 'https', hostname: 'flagcdn.com' },
     ],
     formats: ['image/avif', 'image/webp'],
   },
