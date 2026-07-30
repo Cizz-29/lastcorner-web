73ccaa57f1a8b82523dcb4dfd9bb4d4a49a95baf
Cizz-29 <diblasifra800@gmail.com>
2026-07-30 12:07:27 +0200

    Registra tool Genera Bozza IA in Studio

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,17 +2,24 @@ import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './sanity/schemaTypes'
-
 import { projectId, dataset } from './lib/sanity/env'
+import GeneraBozzaTool from './sanity/tools/GeneraBozzaTool'
 
 export { projectId, dataset }
 
 export default defineConfig({
-    name: 'lastcorner',
-    title: 'Lastcorner CMS',
-    basePath: '/studio',
-    projectId,
-    dataset,
-    plugins: [structureTool(), visionTool()],
-    schema: { types: schemaTypes },
+  name: 'lastcorner',
+  title: 'Lastcorner CMS',
+  basePath: '/studio',
+  projectId,
+  dataset,
+  plugins: [structureTool(), visionTool()],
+  schema: { types: schemaTypes },
+  tools: [
+    {
+      name: 'genera-bozza',
+      title: 'Genera Bozza IA',
+      component: GeneraBozzaTool,
+    },
+  ],
 })
