824c91c9074278950ba343d3f664477f2d7ea542
Cizz-29 <diblasifra800@gmail.com>
2026-08-03 22:48:18 +0200

    Studio: registrare il tool Categorie in blocco

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,6 +4,7 @@ import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './sanity/schemaTypes'
 import { projectId, dataset } from './lib/sanity/env'
 import GeneraBozzaTool from './sanity/tools/GeneraBozzaTool'
+import CategorieInBloccoTool from './sanity/tools/CategorieInBloccoTool'
 import { structure } from './sanity/structure'
 
 export { projectId, dataset }
@@ -27,6 +28,11 @@ export default defineConfig({
       name: 'genera-bozza',
       title: 'Genera Bozza IA',
       component: GeneraBozzaTool,
+    },
+    {
+      name: 'categorie-in-blocco',
+      title: 'Categorie in blocco',
+      component: CategorieInBloccoTool,
     },
   ],
 })
